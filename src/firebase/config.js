e9593a9e03716735249051dfdc218f48a8ac3b62
Anais Moutarlier <anaismoutarlier@gmail.com>
2022-01-24 12:10:23 +0100

    reset config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBMRs1m1TvIl4j-82roIsh0Ln9N2Pj4d_8",
-    authDomain: "lacapsule-masterclass.firebaseapp.com",
-    projectId: "lacapsule-masterclass",
-    storageBucket: "lacapsule-masterclass.appspot.com",
-    messagingSenderId: "186408952490",
-    appId: "1:186408952490:web:ed1aa305200dc344c226d3",
-    measurementId: "G-T19KXX6R3C"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 export default firebaseConfig
